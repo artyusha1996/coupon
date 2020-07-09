32bd403df1ac304d4a480d49d0e9b50522781685
artur <artur.1996.harutyunyan@gmail.com>
2020-07-09 18:04:08 +0400

    Added CouponService for managing business logic of coupon enity

---
+++ app/Services/CouponService.php
@@ -0,0 +1,109 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Events\CouponCreatedEvent;
+use App\Models\Coupon;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use App\Services\ThirdParty\CouponService as CouponThirdPartService;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
+
+class CouponService
+{
+    /**
+     * @var CouponThirdPartService
+     */
+    private $couponService;
+
+    const STATUS_PROCESSING = 'processing';
+    const STATUS_NEW = 'new';
+    const STATUS_USED = 'used';
+    const STATUS_REJECTED = 'rejected';
+
+    const STATUSES_CAN_BE_REGISTERED = [
+        self::STATUS_NEW,
+        self::STATUS_PROCESSING,
+    ];
+
+    /**
+     * CouponService constructor.
+     * @param CouponThirdPartService $couponService
+     */
+    public function __construct(CouponThirdPartService $couponService)
+    {
+        $this->couponService = $couponService;
+    }
+
+    /**
+     * @param string $owner
+     * @param string $code
+     * @return mixed
+     */
+    public function register(string $owner, string $code)
+    {
+        $coupon = $this->firstByCode($code);
+        if ($coupon) {
+            throw new BadRequestException();
+        }
+
+        return $this->create($owner, $code);
+    }
+
+    /**
+     * @param Coupon $coupon
+     * @return Coupon
+     */
+    public function publish(Coupon $coupon)
+    {
+        if (!in_array($coupon->status, self::STATUSES_CAN_BE_REGISTERED)) {
+            throw new BadRequestException();
+        }
+        $coupon->update([
+            'status' => self::STATUS_PROCESSING,
+        ]);
+
+        try {
+            $this->couponService->register($coupon->code);
+            $coupon->update([
+                'status' => self::STATUS_USED,
+            ]);
+        } catch (BadRequestException $exception) {
+            $coupon->update([
+                'status' => self::STATUS_REJECTED,
+            ]);
+        } catch (ServiceUnavailableHttpException $exception) {
+            Log::info('The coupon service answers about server error: ' . $exception->getMessage());
+        }
+
+        return $coupon;
+    }
+
+    /**
+     * @param $code
+     * @return Coupon | null
+     */
+    public function firstByCode(string $code)
+    {
+        return Coupon::where('code', $code)->first();
+    }
+
+    /**
+     * @param string $owner
+     * @param string $code
+     */
+    public function create(string $owner, string $code)
+    {
+        $coupon =  Coupon::create([
+            'code' => $code,
+            'owner' => $owner,
+            'status' => self::STATUS_NEW,
+        ]);
+
+        event(new CouponCreatedEvent($coupon));
+
+        return $coupon;
+    }
+}
---
+++ app/Services/ThirdParty/CouponService.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Services\ThirdParty;
+
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
+
+/**
+ * Class CouponService
+ * @package App\Services\ThirdParty
+ * @method
+ */
+class CouponService
+{
+    public function register(string $code)
+    {
+        $codes = ['200 Success',  '400 Bad Request',  '500 Internal Server Error'];
+        $code = $codes[array_rand($codes)];
+        sleep(rand(1,10));
+        if ($code === $codes[1]) {
+            throw new BadRequestException();
+        } elseif ($code === $codes[2]) {
+            throw new ServiceUnavailableHttpException();
+        }
+    }
+}
